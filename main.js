b101498e9df746ead41c8bb4a604f6650fddba49
abusado-reidelas <buss.mateus@escola.pr.gov.br>
2026-05-11 12:36:35 +0000

    correções

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-<script src="main.js"></script>
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    botoes[i].classList.add("ativo");
-  };
-}
-for (let j = 0; j < botoes.length; j++) {
-  botoes[j].classList.remove("ativo");
-}
+
+botoes.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    botoes.forEach((item) => item.classList.remove("ativo"));
+    botao.classList.add("ativo");
+  });
+});
